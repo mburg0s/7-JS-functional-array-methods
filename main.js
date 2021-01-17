15003cfb393c2f4334b32fe95f81b3561ca64894
mburg0s <burgos.marissa@gmail.com>
2021-01-16 21:31:04 -0500

    updated  main.js

--- main.js
+++ main.js
@@ -21,29 +21,27 @@ const costB=items.filter(function(item) {
     return item.price < 18 && item.price > 14
 })
 
- let html=costB.map(function(x){
+ let answer2=costB.map(function(x){
      return `<div>${x.title}</div>`
 
  }).join('')
- document.querySelector('#answer2').innerHTML=html
+ document.querySelector('#answer2').innerHTML=answer2
 
 
 
 
 // Q3 ------ item has GBP currency code ------
 
-const curGBP= []
-let gbpPrice=0
-items.filter(function(item) {
+let curGBP=items.filter(function(item) {
     // console.log(item.currency_code)
-    return
-    if (item.currency_code =='GBP') {
-        gbpPrice=item.price
-        curGBP.push(item.title)
-    }
+    return item.currency_code =='GBP'
 
 })
-document.querySelector('#answer3').innerHTML=`${curGBP} cost $${gbpPrice}`
+let answer3=curGBP.map(function(gbp){
+    return `<div>${gbp.title} cost $${gbp.price}</div>`
+
+}).join('')
+document.querySelector('#answer3').innerHTML=answer3
 
 
 // Q4 ----- list of all items made of wood ------
@@ -53,30 +51,25 @@ let listWood=items.filter(function(item) {
 
 })
 
-      let html2=listWood.map(function(wood){
+      let answer4=listWood.map(function(wood){
         return `<div>${wood.title} is made of wood</div>`
    
     }).join('')
-    document.querySelector('#answer4').innerHTML=html2
+    document.querySelector('#answer4').innerHTML=answer4
          
 
 //  Q5 ----- list of items made of 8 or more materials ------
-    // let fiveItems =[]
     const itemMaterials=items.filter(function(item){
         return item.materials.length > 8
         
     })
 
-    let html1=itemMaterials.map(function(mtrl){
-        return `<div>${mtrl.title} has ${mtrl.materials.length} materials </div>
-            for (i=0 ; i<mtrl.materials.length ; i++) {
-                '<li>${mtrl.materials}</li>
-                </div>
-            }        
-        
-        
+    let answer5=itemMaterials.map(function(mtrl){
+
+        return `<div>${mtrl.title} has ${mtrl.materials.length} materials
+    ${mtrl.materials}</div>`
          }).join('')
-         document.querySelector('#answer5').innerHTML=html1
+         document.querySelector('#answer5').innerHTML=`${answer5}`
         
 
 
@@ -86,25 +79,4 @@ let totalSeller = items.filter(function(item){
     return item.who_made=='i_did'
     
 })
-console.log(totalSeller)    
     document.querySelector('#answer6').innerHTML= `${totalSeller.length} were made by their sellers`
- 
-    //   if (itemMaterials) {
-    //         fiveItems.push(item.user_id) 
-    //         console.log(item.user_id)
-    //      }
-
-    // console.log(items.materials)
-
-// const htmlThing = items.map(function(items){
-//     return `<li>${items.price}</li>`
-
-    
-
-
-// })
-// document.querySelector ('#answer2').innerHTML=`${htmlThing}`
-
-
-
-// console.log(items[0])
